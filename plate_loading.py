266042b0cdd39d7a8e0d1d80a09694e4b89c748f
Guyzaidman <guyzaidm@gmail.com>
2021-02-27 17:55:58 +0200

    added load and save pickle object functionality

--- plate_loading.py
+++ plate_loading.py
@@ -1,26 +1,8 @@
 import os
-from math import sqrt
 import pandas as pd
 from tqdm import tqdm
 from Well import Well
 import pickle
-
-
-# def DTWDistance(s1, s2):
-#     DTW = {}
-#
-#     for i in range(len(s1)):
-#         DTW[(i, -1)] = float('inf')
-#     for i in range(len(s2)):
-#         DTW[(-1, i)] = float('inf')
-#     DTW[(-1, -1)] = 0
-#
-#     for i in range(len(s1)):
-#         for j in range(len(s2)):
-#             dist = (s1[i] - s2[j]) ** 2
-#             DTW[(i, j)] = dist + min(DTW[(i - 1, j)], DTW[(i, j - 1)], DTW[(i - 1, j - 1)])
-#
-#     return sqrt(DTW[len(s1) - 1, len(s2) - 1])
 
 
 def load_well(path):
@@ -47,64 +29,36 @@ def load_well(path):
 def load_plate(path):
     wells = []
     for root, directories, filenames in os.walk(path):
-        for dir in tqdm(directories):
-            w = load_well(os.path.join(root, dir))
+        for d in tqdm(directories):
+            w = load_well(os.path.join(root, d))
             if w is not None:
                 wells.append(w)
         break
     return wells
 
 
-# p = r'C:\Users\Guyza\OneDrive\Desktop\Information_Systems\Lab_Work\Cell_Communication\Pyproject\Wells\DMSOs\plate_1.1\49'
-p = "C:\\Guy\\ordered_plates\\Plate 1.1\\40506"
-# p = "C:\\Guy\\ordered_plates"
-directories = ['Plate 1.1', 'Plate 1.2', 'Plate 1.3',
-               'Plate 2.1', 'Plate 2.2', 'Plate 2.3',
-               'Plate 3.1', 'Plate 3.2', 'Plate 3.3',]
-
-# directories = ['Plate 2.1']
-# plates = {}
-# for dir in directories:
-#     print("loading " + dir)
-#     plates[dir] = load_plate(os.path.join(p, dir))
-#
-# print('done loading')
-
-# Step 2
-# for k in plates:
-#     n = os.path.join(p, k + '.obj')
-#     with open(n, 'wb') as config_dictionary_file:
-#         # Step 3
-#         pickle.dump(plates[k], config_dictionary_file)
-
-print('Done!!!')
-
-# for k in plates:
-#     for w in plates[k]:
-#         for c in w.cells:
-#             print(c.id)
-#             c.plot_ktr()
-#             print(c.get_cell_position(100))
-#             print(c.number_of_frames)
-#             print("\n------------\n")
+def saving_pickle_obj(path, name, obj):
+    """
+    general purpose function for saving an object using pickle protocol
+    :param path: String, path to desired directory
+    :param name: String, object name
+    :param obj: Object, object to save
+    :return: -
+    """
+    s = os.path.join(path, name + '.obj')
+    with open(s, 'wb') as f:
+        pickle.dump(obj, f)
 
 
-x = load_well(p)
-x.plot_correlation_distribution(n=33)
-print()
+def load_pickle_obj(path, name):
+    """
+    general purpose function for loading an object using pickle protocol
+    :param path: String, path to desired directory
+    :param name: String, object name
+    :return: desired object
+    """
+    s = os.path.join(path, name + '.obj')
+    with open(s, 'rb') as f:
+        o = pickle.load(f)
+        return o
 
-# ---------------------------------------------------
-
-# names = ['405_0' + str(i) for i in range(1, 10)]
-# names.extend(['405_' + str(i) for i in range(10, 51)])
-#
-#
-# for name in names:
-#     for root, directories, filenames in os.walk(p):
-#         for dir in directories:
-#             src = os.path.join(p, dir)
-#             dest = os.path.join(p, dir.replace('_', ''))
-#             os.rename(src, dest)
-#         print('done')
-#
-# print()
